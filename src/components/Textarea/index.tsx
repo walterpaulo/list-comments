d3f3e67dc1b2c7d418df2a6a925747db685aaf66
walterpaulo <walter0paulo@hotmail.com>
2022-06-17 10:10:34 -0300

    fix: count character

--- src/components/Textarea/index.tsx
+++ src/components/Textarea/index.tsx
@@ -4,16 +4,17 @@ import { Texts, MaxLengthTextare, Container } from './style';
 type TextareaProps = {
    width?: string;
    height?: string;
+   numberText?: number;
 } & TextareaHTMLAttributes<HTMLTextAreaElement>
 
-export const Textarea:React.FC<TextareaProps> = ({ children, width, height, maxLength, ...rest }) => {
+export const Textarea:React.FC<TextareaProps> = ({ children, width, height, maxLength, numberText, ...rest }) => {
     return (
         <Container>
             <Texts widht={width} height={height} maxLength={maxLength} {...rest}>
                 {children}
             </Texts>
             <MaxLengthTextare>
-                {maxLength}
+                {maxLength && numberText}
             </MaxLengthTextare>
         </Container>
     );
--- src/pages/Comment/index.tsx
+++ src/pages/Comment/index.tsx
@@ -7,26 +7,32 @@ function Comment() {
 
 	const [ comment, setComment] = useState<string>()
 	const [ listComment, setListComment] = useState([])
-	const [ numberText, setNumeberText] = useState(108)
+	const [ numberText, setNumeberText] = useState<number>()
 	
 	const addComment = ()=>{
 		const listobjet = setListComment
 		setListComment(list=> [...list, comment])
 		setComment("")
+		setNumeberText(108)
+	}
+
+	const handleTextarae = (e: any) =>{
+		const textTextare = e.target.value
+		const maxLength = e.target.maxLength
+		setComment(textTextare)
+		const countCharacter = textTextare.length
+		setNumeberText( maxLength - countCharacter )
 	}
 	
-	useEffect(() => {
-	});
 	return (
 		<Container>
 			<h2>Postar novo comentário</h2>
-				<Textarea width='80%' height='100px' maxLength={numberText}  onChange={(e) => 
-					{setComment(e.target.value), console.log(e.target.value.length)}} value={comment}>
+				<Textarea width='80%' height='100px' maxLength={108} numberText={numberText} onChange={handleTextarae} value={comment}>
 				</Textarea>
 				<Button width='90%' height='36px' onClick={addComment}>Comentar</Button>
 				<ul>
-					{listComment.map(prop=>(
-						<li key={prop}>{prop}</li>
+					{listComment.map((prop, i)=>(
+						<li key={i}>{prop}</li>
 					))}
 				</ul>
 		</Container>
