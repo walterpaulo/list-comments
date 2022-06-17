52cab2bd8b59bfbc3aa60f98d5f8e2af41b7aafa
walterpaulo <walter0paulo@hotmail.com>
2022-06-17 06:33:07 -0300

    refactor: code

--- src/pages/Comment/style.ts
+++ src/pages/Comment/style.ts
@@ -1,12 +1,25 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 414px;
-    height: 100vh;
-    border: 1px solid red;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 10px;
-    background-color: var(--bg-color-primary);
+	width: 414px;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 10px;
+	background-color: var(--bg-color-primary);
+	padding-top: 10px;
+
+	ul{
+		width: 90%;
+		list-style-type: none;
+		
+		li{
+				margin-bottom: 10px;
+				background-color: var(--bg-color-secondary);
+				padding: 20px 5%;
+				border-radius: 5px;
+				color: var(--bg-color)
+		}
+	}
 `
