a4fc21b7948f3b63e220197e19d94c3c63f09c21
walterpaulo <walter0paulo@hotmail.com>
2022-06-17 06:02:20 -0300

    feat: added container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Container } from './app-style'
 import Comment from './pages/Comment'
 
 function App() {
 
   return (
-    <>
+    <Container>
       <Comment />
-    </>
+    </Container>
   )
 }
 
