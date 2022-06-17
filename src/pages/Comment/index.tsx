90a570049d5f27417d48df978b9f7356f6087e9b
walterpaulo <walter0paulo@hotmail.com>
2022-06-17 00:54:35 -0300

    refactor: code

--- src/pages/Comment/index.tsx
+++ src/pages/Comment/index.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '../../components/Button';
 import { Textarea } from '../../components/Textarea';
 import { Container } from './style';
 
 function Comment() {
+
+	const [ comment, setComment] = useState<string>()
+	const [ listComment, setListComment] = useState([])
+	
+	const addComment = ()=>{
+		const listobjet = setListComment
+		setListComment(list=> [...list, comment])
+		setComment("")
+	}
 	return (
 		<Container>
-				<Textarea ></Textarea>
-				<Button>Comentar</Button>
+				<Textarea onChange={(e) => 
+					{setComment(e.target.value)}} value={comment}>
+				</Textarea>
+				<Button onClick={addComment}>Comentar</Button>
 				<ul>
-					<li>Comentário 1</li>
-					<li>Comentário 2</li>
+					{listComment.map(prop=>(
+						<li key={prop}>{prop}</li>
+					))}
 				</ul>
 		</Container>
 	);
