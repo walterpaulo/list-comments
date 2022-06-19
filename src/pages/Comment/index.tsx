98b739478ab3bba950125b27d0197e2bbd80c142
walterpaulo <walter0paulo@hotmail.com>
2022-06-19 00:13:46 -0300

    fix:

--- src/pages/Comment/index.tsx
+++ src/pages/Comment/index.tsx
@@ -7,22 +7,23 @@ import { FiXCircle } from "react-icons/fi";
 function Comment() {
 
 	const [ comment, setComment] = useState<string>()
+	const [ messageExcluir, setmessageExcluir] = useState<string>()
 	const [ listComment, setListComment] = useState([])
 	const [ numberText, setNumeberText] = useState<number>()
   const [ isButton, setIsButton] = useState(false)
   const [ messageDialog, setMessageDialog] = useState(false)
+  const [ indice, setIndice] = useState<number>(0)
 	
 	const addComment = ()=>{
-		const listobjet = setListComment
 		setListComment(list=> [...list, comment])
 		setComment("")
 		setNumeberText(108)
     setIsButton(false)
 	}
-  const setDialogState = () => {
-    setMessageDialog(!messageDialog)
+  const removeComment = () => {
+    listComment.splice(indice)
+    setDialogState()
   }
-
 	const handleTextarae = (e: any) =>{
 		const textTextare = e.target.value
 		const maxLength = e.target.maxLength
@@ -31,36 +32,29 @@ function Comment() {
 		setNumeberText( maxLength - countCharacter )
     countCharacter >= 3? setIsButton(true) : setIsButton(false)
   }
-  const isDialog = (prop:string, id:number) => {
-    console.log(prop, id)
-
-    boxMessage(prop,id)
+  const isDialog = (prop:string, id:any) => {
+    setmessageExcluir(prop)
+    setIndice(id)
     setDialogState()
   }
-
-  const boxMessage = (prop: string, i: number) =>{
+  const setDialogState = () => {
+    setMessageDialog(!messageDialog)
+  }
+  const boxMessage = () =>{
    return(
-    <MessageDialog display="flex">
-          <MessageText>
-            <FiXCircle color="var(--text-color-primary)" size={25} />
-            <p>Apagar?</p>
-            <BoxButtonH>
-              <Button>Cancelar</Button>
-              <Button>Excluir</Button>
-            </BoxButtonH>
-        </MessageText>
+    <MessageDialog display={messageDialog? "flex" : "none"}>
+      <MessageText>
+        <FiXCircle onClick={setDialogState} color="var(--text-color-primary)" size={25} />
+        <p>Apagar?</p>
+        {messageExcluir}
+        <BoxButtonH>
+          <Button onClick={setDialogState}>Cancelar</Button>
+          <Button onClick={removeComment}>Excluir</Button>
+        </BoxButtonH>
+      </MessageText>
     </MessageDialog>
    )
   }
-  // const showBoxMessage = (i: number) => {
-  //   const boxMessage = document.getElementById("idBox"+i.toString())
-  //   // const boxMessage = document.querySelector("#idBox"+i.toString())
-
-  //   // return boxMessage?.setAttribute('display','flex')
-  //   boxMessage?.setAttribute('display', 'flex')
-  //   return boxMessage?.attributes[0].value
-  // }
-  console.log(messageDialog)
 	return (
 		<Container>
       <img src='./images/imagep.jpg' alt='image' />
@@ -76,34 +70,11 @@ function Comment() {
       {listComment.map((prop, i)=>(
         <P key={i}>
           <FiXCircle onClick={()=>{isDialog(prop, i)}} color="var(--text-color-primary)" size={20} />
-          {prop} +{i}
-
-          {/* {boxMessage(prop, i)} */}
-          {/* {console.log('id',showBoxMessage(i))} */}
-
-          {/* {messageDialog && console.log(document.getElementById("idBox"+i.toString()))} */}
-          {/* {messageDialog && 
-          <>
-            {boxMessage(prop, i)}
-            {document.getElementById("idBox"+i.toString())?.setAttribute("display", "none")}
-            <MessageDialog
-            display="flex">
-            </MessageDialog>
-          </>
-          } */}
+          {prop} id: {i}
           </P>
         ))}
       </BoxMessagem>
-      <MessageDialog display={messageDialog? "flex" : "none"}>
-          <MessageText>
-            <FiXCircle color="var(--text-color-primary)" size={25} />
-            <p>Apagar?</p>
-            <BoxButtonH>
-              <Button>Cancelar</Button>
-              <Button>Excluir</Button>
-            </BoxButtonH>
-        </MessageText>
-    </MessageDialog>
+      {boxMessage()}
 		</Container>
 	);
 }
